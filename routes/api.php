b86d82564544c0fe72bb17c2b06c31d7d87e2863
NomanHameed <nomanbutt8322@gmail.com>
2024-02-20 18:27:12 +0500

    done

--- routes/api.php
+++ routes/api.php
@@ -37,7 +37,7 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('kharcha/store', [ExpenseController::class, 'store']);
     Route::get('user/accounts', [UserController::class, 'accounts']);
-    // Route::get('user/expenses', [UserController::class, 'expenses']);
+    Route::get('user/expenses', [UserController::class, 'expenses']);
     Route::get('user/transections', [UserController::class, 'transections']);
     Route::get('fund_transfer/create', [FundTransferController::class, 'create']);
 
