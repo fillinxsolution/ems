fd326aba476d5826b140b1965896976aea58b3e3
Arsal <87937491+arsal214@users.noreply.github.com>
2024-03-22 12:15:52 +0500

    salary month crud

--- app/Http/Controllers/SalaryMonthController.php
+++ app/Http/Controllers/SalaryMonthController.php
@@ -17,7 +17,7 @@ class SalaryMonthController extends Controller
     public function index()
     {
         try {
-            $qualifications = SalaryMonth::where('status', 1)->get();
+            $qualifications = SalaryMonth::where('status', '1')->get();
             return $this->sendResponse($qualifications, 200, ['Get List Successfully.'], true);
         } catch (QueryException $e) {
             Log::error('Database error: ' . $e->getMessage());
@@ -64,26 +64,51 @@ class SalaryMonthController extends Controller
     }
 
     /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
-
-    /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, SalaryMonth $salaryMonth)
     {
-        //
+        $validator = Validator::make($request->all(), [
+            'month' => 'required',
+            'year' => 'required',
+            'name' => 'required',
+            'status' => 'required',
+        ]);
+        $validator->after(function ($validator) use ($request, $salaryMonth) {
+            $exists = SalaryMonth::where('month', $request->month)
+                ->where('year', $request->year)
+                ->exists();
+            if ($exists) {
+                $validator->errors()->add('month', 'The combination of month and year already exists.');
+            }
+        });
+        if ($validator->fails()) {
+            return $this->sendResponse(null, 500, $validator->errors(), false);
+        }
+        try {
+            $salaryMonth->update($request->only(['name', 'month', 'year', 'status']));
+            return $this->sendResponse($salaryMonth, 200, ['Updated Successfully.'], true);
+        } catch (QueryException $e) {
+            Log::error('Database error: ' . $e->getMessage());
+            return $this->sendResponse(null, 500, [$e->getMessage()], false);
+        } catch (\Exception $e) {
+            Log::error('Error: ' . $e->getMessage());
+            return $this->sendResponse(null, 500, [$e->getMessage()], false);
+        }
+
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(SalaryMonth $salaryMonth)
     {
-        //
+        try {
+            $salaryMonth->delete();
+            return $this->sendResponse(null, 200, ['Record deleted successfully.'], true);
+        } catch (\Exception $e) {
+            Log::error('Error: ' . $e->getMessage());
+            return $this->sendResponse(null, 500, [$e->getMessage()], false);
+        }
     }
 }
