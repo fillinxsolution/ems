e277249627f5f42b65ca23377dd203f417a29929
Arsal <87937491+arsal214@users.noreply.github.com>
2024-03-28 12:50:03 +0500

    relation update `

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -219,7 +219,7 @@ class UserController extends Controller
             $request->validate([
                 'user_id' => 'required',
             ]);
-            $importDetail = ImportCsvDetail::where('user_id',$request->user_id)->orderBy('salary_month_id', 'desc')->get();
+            $importDetail = ImportCsvDetail::with('user')->where('user_id',$request->user_id)->orderBy('salary_month_id', 'desc')->get();
             return $this->sendResponse($importDetail, 200, ['Users List'], true);
         } catch (QueryException $e) {
             Log::error('Database error: ' . $e->getMessage());
