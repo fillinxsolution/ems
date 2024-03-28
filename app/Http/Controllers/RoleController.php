36626a23e9d18927db5d8b21c655958933e6fad3
Arsal <87937491+arsal214@users.noreply.github.com>
2024-03-28 16:59:54 +0500

    wip

--- app/Http/Controllers/RoleController.php
+++ app/Http/Controllers/RoleController.php
@@ -21,7 +21,7 @@ class RoleController extends Controller
     public function index()
     {
         try {
-            $roles = Role::with('permissions')->get();
+            $roles = Role::with('permissions')->where('name' ,'!=' ,'Super Admin')->get();
             return $this->sendResponse($roles, 200, ['Roles List'], true);
         } catch (QueryException $e) {
             Log::error('Database error: ' . $e->getMessage());
